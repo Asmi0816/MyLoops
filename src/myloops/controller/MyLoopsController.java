43bd7e04c0485f12ec22c7d9c48a74f921c6c5d7
Asmi0816 <kidwriter2020@gmail.com>
2016-09-20 13:06:47 -0600

    Did a lot with loops and nested if's.

--- src/myloops/controller/MyLoopsController.java
+++ src/myloops/controller/MyLoopsController.java
@@ -2,9 +2,29 @@ package myloops.controller;
 
 public class MyLoopsController {
 	
+	private int countDown;
+	private String tommorow;
+	
 	public void start()
 	{
+		this.countDown = 23;
+		this.tommorow = "Funner than life";
+		while (countDown > 0 )
+		{
+			
+			System.out.println("You have " + countDown + " Seconds left");
+			countDown = countDown - 1;
+		}
+		System.out.println("You're outta time!");
 		
+		for (int index = 23; index >= 0; index--)
+		{
+			if (index == 17)
+			{
+				System.out.println(tommorow);
+			}
+			System.out.println("What does this do really " + index);
+		}
 	}
 	
 }
